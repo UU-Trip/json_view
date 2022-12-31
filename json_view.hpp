dedd00363318a3d5ea3ce4f5395f322afa0fa884
Tripletth <tripletth@users.noreply.github.com>
2022-12-31 02:20:46 +0100

    bloat

--- json_view.hpp
+++ json_view.hpp
@@ -1,16 +1,18 @@
 #pragma once
 
 #include <algorithm>
+#include <array>
 #include <charconv>
 #include <iterator>
 #include <ostream>
+#include <regex>
 #include <string_view>
 #include <type_traits>
 #include <utility>
 
 template <typename char_t> class json_view {
     template <typename enum_class_t>
-    constexpr static auto e_val(enum_class_t&& val) {
+    constexpr inline static auto e_val(enum_class_t&& val) {
         return static_cast<std::underlying_type_t<std::decay_t<enum_class_t>>>(std::forward<enum_class_t>(val));
     }
     enum class syntax : char_t {
@@ -74,6 +76,7 @@ template <typename char_t> class json_view {
                         }
                         ++pos;
                     }
+                    return {data, pos};
                 }
             }
             return {data};
@@ -103,7 +106,10 @@ template <typename char_t> class json_view {
         using reference = void;
         using pointer = void;
         constexpr iterator(std::basic_string_view<char_t> data)
-        : data{data}, index{std::min(data.size(), fetch(data).first() + 1)}
+        : data{data}, index{std::min(data.size(), static_cast<std::size_t>(fetch(data).first().next()))}
+        {}
+        constexpr iterator(std::basic_string_view<char_t> data, std::size_t index)
+        : data{data}, index{index}
         {}
         constexpr json_view operator*() const {
             return data.substr(std::min(data.size(), index), ((((++(iterator(*this))).index - 1) - index)));
@@ -148,9 +154,7 @@ public:
         return data;
     }
     constexpr iterator end() const {
-        iterator it{data};
-        it.index = data.size();
-        return it;
+        return {data, data.size()};
     }
     constexpr std::size_t size() const {
         return std::distance(begin(), end());
@@ -213,23 +217,51 @@ public:
         return (string_view() == "true");
     }
     constexpr int64_t integer() const {
-        int64_t result{0};
-        if (!data.empty()) {
+        if constexpr (std::is_same_v<char_t, char>) {
             auto str{string_view()};
-            if constexpr (std::is_same_v<char_t, char>) {
-                std::from_chars(str.begin(), str.end(), result);
-            } else {
-                result = std::stoll(string());
+            int64_t result{0};
+            std::from_chars(str.begin(), str.end(), result);
+            return result;
+        } else {
+            if (is_integer()) {
+                return std::stoll(string());
             }
-        }
-        return result;
-    }
-    constexpr float floating_point() const {
-        try {
-            return std::stof(string());
-        } catch(std::exception& e) {
             return 0;
         }
+    }
+    constexpr float floating_point() const {
+        if constexpr (std::is_same_v<char_t, char>) {
+            auto str{string_view()};
+            float result{0};
+            std::from_chars(str.begin(), str.end(), result);
+            return result;
+        } else {
+            if (is_floating_point()) {
+                return std::stof(string());
+            }
+            return 0;
+        }
+    }
+    constexpr bool is_object() const {
+        return (!data.empty()) && (data.front() == e_val(syntax::object_open));
+    }
+    constexpr bool is_array() const {
+        return (!data.empty()) && (data.front() == e_val(syntax::array_open));
+    }
+    constexpr bool is_string() const {
+        return (key().empty()) && (!data.empty()) && (data.front() == e_val(syntax::string_delimiter));
+    }
+    constexpr bool is_boolean() const {
+        constexpr literal_helper r{"true|false"};
+        return std::regex_match(data.begin(), data.end(), std::basic_regex<char_t>{r});
+    }
+    constexpr bool is_integer() const {
+        constexpr literal_helper r{"^-?\\d+"};
+        return std::regex_match(data.begin(), data.end(), std::basic_regex<char_t>{r});
+    }
+    constexpr bool is_floating_point() const {
+        constexpr literal_helper r{"^[-]?(0|[1-9][0-9]*)(\\.[0-9]+)([eE][+-]?[0-9]+)?$"};
+        return std::regex_match(data.begin(), data.end(), std::basic_regex<char_t>{r});
     }
     constexpr operator std::basic_string<char_t>() const {
         return string();
@@ -238,8 +270,23 @@ public:
         return os << rhs.data;
     }
 private:
+    template <std::size_t n> struct literal_helper {
+        constexpr literal_helper(const char (&in)[n])
+        : str{} {
+            for (std::size_t i{0}; i < n; ++i) {
+                str[i] = static_cast<char_t>(in[i]);
+            }
+        }
+        constexpr operator const char_t*() const {
+            return str.data();
+        }
+        std::array<char_t, n + 1U> str;
+    };
     constexpr std::basic_string_view<char_t> substr(std::size_t first, std::size_t last) const {
         return data.substr(first, std::min(data.size(), last + 1) - first);
+    }
+    constexpr bool empty() const {
+        return data.empty();
     }
 private:
     std::basic_string_view<char_t> data;
